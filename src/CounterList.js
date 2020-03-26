d3fe72e30a57e195bd31cd8fe139a09158fbf1ad
jnch009 <jngcheng@sfu.ca>
2020-03-26 15:57:46 -0700

    Playing with persistedState

--- src/CounterList.js
+++ src/CounterList.js
@@ -9,7 +9,19 @@ import todoApp from "./reducers/reducersTodoList";
 import "./styles.css";
 
 export default function CounterList() {
+  // const persistedState = {
+  //   todos: [
+  //     {
+  //       id: 0,
+  //       text: "Welcome Back",
+  //       completed: true
+  //     }
+  //   ],
+  //   visiblityFilter: "SHOW_COMPLETED"
+  // };
+
   const store = createStore(todoApp);
+  console.log(store.getState());
   ReactDOM.render(
     <Provider store={store}>
       <TodoApp />
