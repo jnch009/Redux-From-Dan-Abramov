39558d13c22ff4444f1543ad72650364617c470a
jnch009 <jngcheng@sfu.ca>
2020-03-27 20:38:24 -0700

    Refactoring for react-router

---
+++ src/ConfigureStore.js
@@ -0,0 +1,24 @@
+import throttle from "lodash/throttle";
+import { createStore } from "redux";
+
+import { loadState, saveState } from "./localStorage";
+import todoApp from "./reducers/reducersTodoList";
+
+import "./styles.css";
+
+const configureStore = () => {
+  const persistedState = loadState();
+  const store = createStore(todoApp, persistedState);
+
+  store.subscribe(
+    throttle(() => {
+      saveState({ todos: store.getState().todos });
+    }, 1000)
+  );
+
+  console.log(store.getState());
+
+  return store;
+};
+
+export default configureStore;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import CounterList from "./CounterList";
-CounterList();
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Root from "./components/presentational/Root";
+import configureStore from "./ConfigureStore";
+
+const store = configureStore();
+
+ReactDOM.render(<Root store={store} />, document.getElementById("root"));
